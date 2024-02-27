329429d59045695df3bb22b83bdd0c7152cb636e
Ahmed Arefin <ahmedarefinsajjad@gmail.com>
2024-02-27 23:57:50 +0600

    sunny: Update

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -32,41 +32,33 @@ if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
 fi
 
 if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
-	export OF_USE_GREEN_LED=0
 	export FOX_ENABLE_APP_MANAGER=1
-	export OF_IGNORE_LOGICAL_MOUNT_ERRORS=1
 	export TW_DEFAULT_LANGUAGE="en"
 	export LC_ALL="C"
 	export ALLOW_MISSING_DEPENDENCIES=true
 	export TARGET_DEVICE_ALT="sunny"
+        export FOX_VANILLA_BUILD=1
 	export FOX_TARGET_DEVICES="mojito,sunny"
 	export FOX_VIRTUAL_AB_DEVICE=1
 	export FOX_RECOVERY_SYSTEM_PARTITION="/dev/block/mapper/system"
 	export FOX_RECOVERY_VENDOR_PARTITION="/dev/block/mapper/vendor"
-	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
-	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
-	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
 	export FOX_USE_BASH_SHELL=1
 	export FOX_ASH_IS_BASH=1
 	export FOX_USE_TAR_BINARY=1
 	export FOX_USE_SED_BINARY=1
 	export FOX_USE_XZ_UTILS=1
-	export OF_ENABLE_LPTOOLS=1
 	export FOX_USE_NANO_EDITOR=1
-	export OF_QUICK_BACKUP_LIST="/boot;/data;"
-	export FOX_DELETE_AROMAFM=1
+        export FOX_VARIANT="vBaR"
 	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
 	export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk.zip
-        export FOX_VENDOR_BOOT_RECOVERY=1
 
 	# Ensure that /sdcard is bind-unmounted before f2fs data repair or format
 	export OF_UNBIND_SDCARD_F2FS=1
 
 	# Instruct magiskboot v24+ to always patch the vbmeta header when patching the recovery/boot image; do *not* remove!
-        export FOX_PATCH_VBMETA_FLAG="1"
+        export OF_PATCH_VBMETA_FLAG="1"
 
 	# No special MIUI stuff
-        export FOX_VANILLA_BUILD=1
 	export OF_NO_MIUI_PATCH_WARNING=1
 
 	# Full Dynamic Partition size
@@ -80,11 +72,10 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
 	export OF_CLOCK_POS=1
 	export OF_HIDE_NOTCH=1
 	export OF_ALLOW_DISABLE_NAVBAR=0
-
-	# R12.1 Settings
-	export FOX_VERSION="R12.1"
+	
+	# R11.1 Settings
+	export FOX_VERSION="R11.1_1"
 	export FOX_BUILD_TYPE="Beta"
-	export OF_MAINTAINER="Arefin"
 
 	# Magisk
 	if [ -n "${FOX_USE_SPECIFIC_MAGISK_ZIP}" ]; then
